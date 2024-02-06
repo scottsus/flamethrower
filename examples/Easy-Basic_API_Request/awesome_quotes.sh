d93b3e434a4469f9e5f7be3ac64a3db5d14169a0
scott susanto <susantoscott@gmail.com>
2024-02-06 11:51:12 -0800

    edit examples

---
+++ examples/Easy-Basic_API_Request/awesome_quotes.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+# This shell script prints out awesome quotes with emojis
+
+# Quote 1
+echo -e "💡 'Knowledge is power.' - Francis Bacon"
+
+# Quote 2
+echo -e "👊 'With great power comes great responsibility.' - Uncle Ben"
+
+# Quote 3
+echo -e "🚀 'To infinity and beyond!' - Buzz Lightyear"
+
+# Quote 4
+echo -e "🧗 'It is not the mountain we conquer, but ourselves.' - Sir Edmund Hillary"
+
+# Quote 5
+echo -e "🌟 'Be yourself; everyone else is already taken.' - Oscar Wilde"
+
+# End of script
+
--- examples/Easy-Basic_API_Request/chat.py
+++ examples/Easy-Basic_API_Request/chat.py
@@ -5,8 +5,9 @@ res = model.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "You are humorous and love emojis."},
-        {"role": "user", "content": "What is the meaning of life?"},
+        {"role": "user", "content": "What is the meaning of life?"},  # Changed 'human' to 'user'
     ]
 )
 
-print(res.choices[0].message.content)  # Corrected access to message content
+# I assume the correct attribute names to access the message should be 'choices[0].message.content' rather than 'choices.message.content'
+print(res.choices[0].message.content)  # Modified the line to correctly index into the response
